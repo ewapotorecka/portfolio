b16dab21af5f312fb62532e3c847162941f60af6
Ewa Potorecka <ewa.potorecka@gmail.com>
2020-12-12 17:44:16 +0100

    Updates

---
+++ precache-manifest.1252853a15bae841a6efedf42f78b938.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62327c162e7a3ec97b8231c3af2be252",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1da6f48add2f2c06015c",
+    "url": "/portfolio/static/css/main.6f4cc6f5.chunk.css"
+  },
+  {
+    "revision": "ccf240d9024a7d669b78",
+    "url": "/portfolio/static/js/2.262e7550.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/portfolio/static/js/2.262e7550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da6f48add2f2c06015c",
+    "url": "/portfolio/static/js/main.0a3b8d11.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "94a251e076ba6953fd5a2b4d01af7e80",
+    "url": "/portfolio/static/media/climbing.94a251e0.png"
+  },
+  {
+    "revision": "5bcc0ef99cf1bd629be765227e226738",
+    "url": "/portfolio/static/media/pigGame.5bcc0ef9.png"
+  },
+  {
+    "revision": "6f2bd4f87f6a7545a8c7b10564c98540",
+    "url": "/portfolio/static/media/poodle.6f2bd4f8.png"
+  },
+  {
+    "revision": "a1a5336c7414b062d0a6b91c9beb59c0",
+    "url": "/portfolio/static/media/profile.a1a5336c.jpg"
+  },
+  {
+    "revision": "82fb9b40e0cc93c5d25f3f86c5c5136a",
+    "url": "/portfolio/static/media/run.82fb9b40.png"
+  }
+]);
